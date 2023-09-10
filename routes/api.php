d4d02fa8e833d37fac002426b4a66ad91294eb94
Leopoldo Jacob Matsinhe <121976617+l0m2@users.noreply.github.com>
2023-09-10 14:51:29 +0200

    routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MovimentosController;
+use App\Http\Controllers\FornecerController;
+use App\Http\Controllers\ItensPedidoCompraController;
+use App\Http\Controllers\PedidoCompraController;
+use App\Http\Controllers\ProdutoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +19,22 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
+/*
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});*/
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login',[AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function(){
+Route::apiResources([
+    'produto' => ProdutoController::class,
+    'fornecedor' => FornecerController::class,
+    'pedido_compra' => PedidoCompraController::class,
+    'itens_pedido_compra' => ItensPedidoCompraController::class 
+]);
+Route::get('/movimentos',[MovimentosController::class, 'getAll']);
 });
+
+
