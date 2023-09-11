61e66eac70f094063cb0ffd463c0d75269506655
Leopoldo Jacob Matsinhe <121976617+l0m2@users.noreply.github.com>
2023-09-12 00:04:04 +0200

    model itens pedido compra

--- app/Models/itensPedidoCompra.php
+++ app/Models/itensPedidoCompra.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\pedidoCompra;
+use App\Models\produto;
 
 class itensPedidoCompra extends Model
 {
     use HasFactory;
+
+    public function pedidoCompra()
+    {
+        return $this->belongsTo(pedidoCompra::class, 'pedido_compra_id');
+    }
+
+    public function produto()
+    {
+        return $this->belongsTo(produto::class, 'produto_id');
+    }
+
+    protected $table='itens_pedido_compras';
+
+    protected $fillable = [
+        'pedido_compra_id',
+       'produto_id',
+        'quantidade',
+        'preco_unitario'
+    ];
 }
