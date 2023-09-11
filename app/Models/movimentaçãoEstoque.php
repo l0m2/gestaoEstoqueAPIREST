d2043609d0a88ebed64c3fddc269cc2e84a71e10
Leopoldo Jacob Matsinhe <121976617+l0m2@users.noreply.github.com>
2023-09-12 00:08:52 +0200

    model movimentação_estoques

--- app/Models/movimentaçãoEstoque.php
+++ app/Models/movimentaçãoEstoque.php
@@ -4,8 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\produto;
+use App\Models\User;
 
 class movimentaçãoEstoque extends Model
 {
     use HasFactory;
+
+    public function produto()
+    {
+        return $this->belongsTo(produto::class, 'produto_id');
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    protected $table='movimentação_estoques';
+
+    protected $fillable = [
+       'user_id',
+        'produto_id',
+        'tipo_movimentacao', 
+        'quantidade'
+    ];
 }
