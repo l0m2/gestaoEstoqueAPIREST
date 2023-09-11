172bfca389d4e2d799d8047ee415def0432f6eb7
Leopoldo Jacob Matsinhe <121976617+l0m2@users.noreply.github.com>
2023-09-11 23:53:49 +0200

    model fornecer

--- app/Models/fornecer.php
+++ app/Models/fornecer.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class fornecer extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+       'firstname',
+       'lastname',
+       'contacto',
+       'endereco'
+      ];
 }
