fdf075e95dd18c4a76cfc895be6398f6e1b46ac5
Leopoldo Jacob Matsinhe <121976617+l0m2@users.noreply.github.com>
2023-09-23 20:37:00 +0200

    pedido compra update

--- app/Http/Controllers/PedidoCompraController.php
+++ app/Http/Controllers/PedidoCompraController.php
@@ -90,9 +90,57 @@ class PedidoCompraController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, pedidoCompra $pedidoCompra)
+    public function update(Request $request, $id)
     {
-        //
+        $pedidoCompra = pedidoCompra::find($id);
+        if(!$pedidoCompra){
+            return response()->json([
+             'message'=> 'Pedido de Compra nao existe'
+            ],404);
+        }
+
+        $validator = Validator::make(
+            $request->all(),[
+                'fornecedor_id' => 'required|integer|min:0',
+                'status'=> 'required|string|max:15'  
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'message'=> 'Erro na validacao dos dados'
+            ], 400);
+        }
+
+        else if(!fornecer::find($request['fornecedor_id'])){
+            return response()->json([
+                'message' => 'Fornecedor nao existe'
+            ],404);
+          }  
+
+
+          $validated = $validator->validated();
+          
+         if($validated['status'] == 'concluido'){
+            return response()->json([
+    'message'=> 'Nao e possivel concluir um pedido sem que nenhum produto seja comprado'
+            ],400);
+        }
+          
+        $update = $pedidoCompra ->update([
+        'fornecedor_id' => $validated['fornecedor_id'],
+        'status' => $validated['status']
+        ]);
+
+        if($update){
+            return response()->json([
+                'message'=> 'Pedido de Compra atualizado'
+            ],200);
+        }
+
+        return response()->json([
+            'message' => 'Erro na atualizacao do Pedido de Compra'
+        ], 400);
+
     }
 
     /**
